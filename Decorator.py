cd7cb86ae11c5cb66073ed196005ed05f35a276e
RAÚL ARROYO TORRIJOS <Raul.Arroyo1@alu.uclm.es>
2023-02-15 11:50:47 +0100

    primera version

---
+++ Decorator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from ElementoMapa import ElementoMapa
+
+class Decorator(ElementoMapa):
+    def __init__(self):
+        self.activa = None
+
---
+++ Habitación.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from ElementoMapa import ElementoMapa
+
+class Habitación(ElementoMapa):
+    def __init__(self):
+        self.norte = None
+
